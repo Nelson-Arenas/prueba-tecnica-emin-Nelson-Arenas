eb1bc445ba01618a721a1bd86cac6129af189ea0
Nelson-Arenas <racacaca_@hotmail.com>
2026-01-24 02:48:10 -0300

    Upadate email y password schema

--- src/models/User.ts
+++ src/models/User.ts
@@ -2,8 +2,8 @@ import mongoose, {Schema} from "mongoose";
 
 const userSchema = new Schema({
     name: { type: String, required: true },
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
+    email: { type: String, required: true, unique: true, trim: true },
+    password: { type: String, required: true , trim: true },
 }, { timestamps: true });
 
 userSchema.index({ email: 1 }, { unique: true });
