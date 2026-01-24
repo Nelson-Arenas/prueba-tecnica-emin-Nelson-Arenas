a9aa8c716ef4aa382266d5724c9a7797b1195931
Nelson-Arenas <racacaca_@hotmail.com>
2026-01-24 15:45:47 -0300

    Validaciones register user

--- src/handlers/userHandler.ts
+++ src/handlers/userHandler.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from "express";
+import { validationResult } from "express-validator";
 import { User } from "../models/User";
 import { hashPassword } from "../utils/auth";
 
@@ -7,6 +8,11 @@ export const registerUser = async (req: Request, res: Response) => {
     const userExists = await User.findOne({ email: req.body.email });
     if (userExists) {
       return res.status(409).json({ message: 'El usuario ya existe' });
+    }
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
 
     const hashedPassword = await hashPassword(req.body.password);
--- src/router.ts
+++ src/router.ts
@@ -1,11 +1,16 @@
 import {Router} from 'express';
-import { registerUser } from './handlers';
-
+import { body } from 'express-validator';
+import { registerUser } from './handlers/userHandler';
 
 
 const router = Router();
 
-
-router.post('/auth/register', registerUser);
+router.post('/auth/register', 
+    body('name').notEmpty().withMessage('El nombre es obligatorio'),
+    body('lastName').notEmpty().withMessage('El apellido es obligatorio'),
+    body('email').isEmail().withMessage('El email no es válido'),
+    body('password').isLength({ min: 6 }).withMessage('La contraseña debe tener al menos 6 caracteres'),
+    registerUser
+);
 
 export default router;
