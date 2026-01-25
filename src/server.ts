5458d11f7809055b161e3af9ad93c7ffd3f68eb4
Nelson-Arenas <racacaca_@hotmail.com>
2026-01-25 02:08:10 -0300

    Add cors

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,19 @@
 import express from 'express'
 import 'dotenv/config';
+import cors from 'cors';
 import router from './router';
 import { connectToDatabase } from './config/db';
+import { corsOptions } from './config/cors';
 
 const app = express();
 
 connectToDatabase();
+
+//cors
+app.use(cors(corsOptions));
+
+
+//APP
 app.use(express.json());
 
 app.use('/' , router);
